a77b31ed2c6b698138ce5c9c99a350644c03d312
Radosław <radoslaw.kiszka@edu.uekat.pl>
2026-06-10 22:05:20 +0200

    Update ChessBot.py

--- ChessBot.py
+++ ChessBot.py
@@ -15,12 +15,12 @@ def main():
     try:
         while True:
             msg = peer.recv()
-            print("RECEIVED:", msg)
+            print("Biały:", msg)
 
             board.move_piece(msg)
 
             move = engine.choose_move(board)
-            print("SENDING:", move)
+            print("Czarny:", move)
 
             if move:
                 board.move_piece(move)
